da6cceff4d54152dba5c173d5d88178fcd46c312
Huang,Katie <khua86@gmail.com>
2018-07-21 22:21:11 -0500

    more to the isbn to id and show book calls and book class

--- share-the-things/src/main/java/com/cerner/engineering/ReadISBN.java
+++ share-the-things/src/main/java/com/cerner/engineering/ReadISBN.java
@@ -1,6 +1,7 @@
 package com.cerner.engineering;
 
 import com.cerner.common.collection.SetUtils;
+import com.cerner.engineering.object.Book;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,47 +10,93 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class ReadISBN
 {
     private static final String API_KEY = "vAk3StDKFQ1FtKXzpzo9g";
     private static final String BASE_URL = "https://www.goodreads.com/book/";
     private static final String ISBN_TO_ID = "isbn_to_id";
+    private static final String SHOW_BOOK = "show";
+    private static final String SEPARATOR = ",";
+    private static final String ISBN_TO_ID_RESPONSE_SEPARATOR = "\n";
 
     private static HttpURLConnection con;
 
-    public static void main(final String[] args)
+    public static void main(final String[] args) throws MalformedURLException, ProtocolException, IOException
     {
         try
         {
-            // callShit();
+            // callResource();
         }
         catch (final Exception e)
         {
             e.printStackTrace();
         }
 
-        final Set<Long> isbns = SetUtils.newSet(123L, 1233L);
+        final Set<String> isbns = SetUtils.newSet("0590353403");
+        // final Set<String> isbns = SetUtils.newSet("0441172717", "0590353403", "0739467352"); // need to figure out multiple isbns
         final Set<Long> ids = getIdsFromISBNs(isbns);
+        final Set<Book> books = getBookInfoFromIds(ids);
         System.out.println(ids);
     }
 
-    private static Set<Long> getIdsFromISBNs(final Set<Long> isbns)
+    private static Set<Long> getIdsFromISBNs(final Set<String> isbns) throws MalformedURLException, ProtocolException, IOException
     {
         // https://www.goodreads.com/book/isbn_to_id/0590353403?key=vAk3StDKFQ1FtKXzpzo9g
+
+        final StringBuilder concatenatedIsbnsBuilder = new StringBuilder();
+        for (final String isbn : isbns)
+        {
+            concatenatedIsbnsBuilder.append(isbn);
+            concatenatedIsbnsBuilder.append(SEPARATOR);
+        }
+
+        String concatenatedIsbns = concatenatedIsbnsBuilder.toString();
+        concatenatedIsbns = concatenatedIsbns.substring(0, concatenatedIsbns.length() - SEPARATOR.length());
+
+        System.out.println(concatenatedIsbns);
+
+        final String response = callResource(ISBN_TO_ID, concatenatedIsbns);
+
+        final Set<Long> goodReadsIds = new HashSet();
+        final StringTokenizer st = new StringTokenizer(response, ISBN_TO_ID_RESPONSE_SEPARATOR);
+        while (st.hasMoreTokens())
+        {
+            goodReadsIds.add(Long.valueOf(st.nextToken()));
+        }
+
+        return goodReadsIds;
+    }
+
+    private static Set<Book> getBookInfoFromIds(final Set<Long> goodReadsBookIds) throws MalformedURLException, ProtocolException, IOException
+    {
+        // https://www.goodreads.com/book/show/29409154?format=xml&key=YOURAPIKEY
+
+        final StringBuilder concatenatedIdsBuilder = new StringBuilder();
+        for (final Long goodReadsBookId : goodReadsBookIds)
+        {
+            concatenatedIdsBuilder.append(goodReadsBookId.toString());
+            concatenatedIdsBuilder.append(SEPARATOR);
+        }
+
+        String concatenatedIds = concatenatedIdsBuilder.toString();
+        concatenatedIds = concatenatedIds.substring(0, concatenatedIds.length() - SEPARATOR.length());
+
+        System.out.println(concatenatedIds);
+
+        callResource(SHOW_BOOK, concatenatedIds);
         return null;
     }
 
-    private static void getBookInfoFromIds()
+    public static String callResource(final String resource, final String idParameters) throws MalformedURLException, ProtocolException, IOException
     {
-        // https://www.goodreads.com/book/show/29409154?format=xml&key=YOURAPIKEY
-    }
+        // final String url = "https://www.goodreads.com/book/isbn_to_id/0590353403,0441172717?key=vAk3StDKFQ1FtKXzpzo9g";
 
-    public static String callShit(final String resource, final Set<Long> ids) throws MalformedURLException, ProtocolException, IOException
-    {
-        final String url = "https://www.goodreads.com/book/isbn_to_id/0590353403?key=vAk3StDKFQ1FtKXzpzo9g";
-        final String urlz = new StringBuilder().append(BASE_URL).append(resource).append("/").toString();
+        final String url = new StringBuilder().append(BASE_URL).append(resource).append("/").append(idParameters).append("?key=vAk3StDKFQ1FtKXzpzo9g")
+                .toString();
 
         try
         {
@@ -63,13 +110,16 @@ public class ReadISBN
                 String line;
                 content = new StringBuilder();
 
+                System.out.println(in.toString());
+
                 while ((line = in.readLine()) != null)
                 {
-                    content.append(line);
+                    content.append(line); // eh eh?
                     content.append(System.lineSeparator());
                 }
             }
 
+            System.out.println(content.toString());
             return content.toString();
         }
         finally
@@ -77,5 +127,4 @@ public class ReadISBN
             con.disconnect();
         }
     }
-
 }
